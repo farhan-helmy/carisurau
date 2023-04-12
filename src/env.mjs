0a853db5907a315c917a111a858887d403aa7cae
farhan-helmy <farhanhlmy@gmail.com>
2023-04-12 18:30:15 +0800

    feat: change s3 url to cloudfront dist url

--- src/env.mjs
+++ src/env.mjs
@@ -31,6 +31,7 @@ const server = z.object({
  */
 const client = z.object({
   // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
+  NEXT_PUBLIC_CLOUDFRONT_URL: z.string().url(),
 });
 
 /**
@@ -45,6 +46,7 @@ const processEnv = {
   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
   DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+  NEXT_PUBLIC_CLOUDFRONT_URL: process.env.NEXT_PUBLIC_CLOUDFRONT_URL,
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
 
