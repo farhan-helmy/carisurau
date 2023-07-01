0a120387a0001f4c716867f80cd522780594abc3
Abbas <muhdabbas98@gmail.com>
2023-07-01 13:57:21 +0800

    Separate the Perak Mall database seed. Add optional unique name repair, and fix missing dashes for the next space occurrence.

---
+++ prisma/repair.ts
@@ -0,0 +1,34 @@
+import { prisma } from '../src/server/db';
+
+const repairMallUniqueName = async () => {
+  const mall = await prisma.mall.findMany();
+  for (const m of mall) {
+    const uniqueName = m.name.toLocaleLowerCase().replace(/\s/g, '-');
+    await prisma.mall.update({
+      where: {
+        id: m.id,
+      },
+      data: {
+        value: uniqueName,
+      },
+    });
+  }
+};
+
+const repairDistrictUniqueName = async () => {
+  const district = await prisma.district.findMany();
+  for (const d of district) {
+    const uniqueName = d.name.toLocaleLowerCase().replace(/\s/g, '-');
+    await prisma.district.update({
+      where: {
+        id: d.id,
+      },
+      data: {
+        unique_name: uniqueName,
+      },
+    });
+  }
+};
+
+void repairMallUniqueName();
+void repairDistrictUniqueName();
