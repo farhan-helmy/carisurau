daa95518bfd8943cdff9e37a5327d2e95cb83d23
farhan-helmy <farhanhlmy@gmail.com>
2023-07-23 20:55:01 +0800

    yarn lint

--- src/pages/api/magic-link/approve.ts
+++ src/pages/api/magic-link/approve.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next';
 import {prisma} from '../../../server/db';
 import { Buffer } from 'buffer';
 
