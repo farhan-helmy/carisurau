bfd8eca27241a6707e9de77be3759b8ed68286b0
farhan-helmy <farhanhlmy@gmail.com>
2023-08-30 22:37:19 +0800

    chore: cleanup

--- src/server/api/routers/rate.ts
+++ src/server/api/routers/rate.ts
@@ -1,6 +1,5 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "../trpc";
-import type { Rating } from "@prisma/client"
 
 export const rateRouter = createTRPCRouter({
   addRating: publicProcedure
